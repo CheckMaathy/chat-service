bdda60e338d8cafce40520aeb628698eb4068c2d
checkmaathy <67769338+CheckMaathy@users.noreply.github.com>
2023-04-22 23:47:25 -0300

    feat: add configs to chat-service

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/checkmaathy/chatservice
+
+go 1.19
+
+require (
+	github.com/google/uuid v1.3.0
+	github.com/pkoukk/tiktoken-go v0.1.1
+)
+
+require github.com/dlclark/regexp2 v1.8.1 // indirect
---
+++ internal/domain/entity/chat.go
@@ -0,0 +1,63 @@
+package entity
+
+import "errors"
+
+type ChatConfig struct {
+	Model            *Model
+	Temperature      float32
+	TopP             float32
+	N                int
+	Stop             []string
+	MaxTokens        int
+	PresencePenalty  float32
+	FrequencyPenalty float32
+}
+
+type Chat struct {
+	ID                   string
+	UserID               string
+	InitialSystemMessage *Message
+	Messages             []*Message
+	ErasedMessages       []*Message
+	Status               string
+	TokenUsage           int
+	Config               *ChatConfig
+}
+
+func (c *Chat) AddMessage(m *Message) error {
+	if c.Status == "ended" {
+		return errors.New("chat is endend, no more messages allowed")
+	}
+
+	for {
+		if c.Config.Model.GetMaxTokens() >= m.GetQtdTokens()+c.TokenUsage {
+			c.Messages = append(c.Messages, m)
+			c.RefreshTokenUsage()
+			break
+		}
+		c.ErasedMessages = append(c.ErasedMessages, c.Messages[0])
+		c.Messages = c.Messages[1:]
+		c.RefreshTokenUsage()
+	}
+	return nil
+}
+
+func (c *Chat) RefreshTokenUsage() {
+	c.TokenUsage = 0
+	for m := range c.Messages {
+		c.TokenUsage += c.Messages[m].GetQtdTokens()
+		break
+	}
+}
+
+func (c *Chat) GetMessages() []*Message {
+	return c.Messages
+}
+
+func (c *Chat) CountMessages() int {
+	return len(c.Messages)
+}
+
+func (c *Chat) End() {
+	c.Status = "ended"
+}
